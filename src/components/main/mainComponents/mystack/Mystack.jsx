b8919926c4aa6b13917a8443f521384b72898b94
mountok <themountok@gmail.com>
2022-04-21 16:01:54 +0300

    none

--- src/components/main/mainComponents/mystack/Mystack.jsx
+++ src/components/main/mainComponents/mystack/Mystack.jsx
@@ -5,7 +5,7 @@ const Mystack = () => {
   return (
     <section className={styles.mystack_page}>
         <header className={styles.mystack_page_header}>
-            {/* <p>What can i do ?</p> */}
+            <p>My Services</p>
         </header>
     </section>
   )
