c30d10a29364621992bd11d56358206f48f7a8fa
mountok <themountok@gmail.com>
2023-07-21 22:55:46 +0300

    none

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
         <article className={styles.header_logo}>
-            <h1>.code</h1>
+            <h1>coder</h1>
         </article>
 
         
