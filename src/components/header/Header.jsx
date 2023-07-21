cc1a1b468ec7f665ae18f6ba71dcd1288f51f61f
mountok <themountok@gmail.com>
2023-07-21 22:40:09 +0300

    none

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,13 +7,7 @@ const Header = () => {
             <h1>.code</h1>
         </article>
 
-        <nav className={styles.header_nav}>
-            <a href="#home">Home</a>
-            <a href="#home">About me</a>
-            <a href="#home">My stack</a>
-            <a href="#home">Expirence</a>
-        </nav>
-
+        
         <article className={styles.header_cont}>
             <a href="#contacts">CONTACT</a>
         </article>
