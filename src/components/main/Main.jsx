6a9a58133ef50c46d0618c71c113e556d199ef69
mountok <themountok@gmail.com>
2023-07-22 14:53:31 +0300

    update

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './Main.module.css'
 import Aboutme from './mainComponents/aboutme/Aboutme'
+import Mystack from './mainComponents/mystack/Mystack'
 const Main = () => {
   return (
     <main className={styles.main}>
         <Aboutme />
+        <Mystack />
     </main>
   )
 }
---
+++ src/components/main/mainComponents/mystack/Mystack.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Mystack.module.css'
+
+const Mystack = () => {
+  return (
+    <section className={styles.mystack_page}>
+        <header className={styles.mystack_page_header}>
+            {/* <p>What can i do ?</p> */}
+        </header>
+    </section>
+  )
+}
+
+export default Mystack
