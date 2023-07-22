ed5e4c8eea26754e16f33bec85ba78a40f0ecd4e
mountok <themountok@gmail.com>
2023-07-22 19:35:44 +0300

    none

--- src/components/main/mainComponents/mystack/Mystack.jsx
+++ src/components/main/mainComponents/mystack/Mystack.jsx
@@ -6,7 +6,39 @@ const Mystack = () => {
     <section className={styles.mystack_page}>
         <header className={styles.mystack_page_header}>
             <p>My Services</p>
+
         </header>
+        <main className={styles.mystack_page_main}>
+          <div className={styles.mystack_page_main_item}>
+            <header className={styles.mystack_page_main_item_header}>
+
+            </header>
+            <content style={styles.mystack_page_main_item_content}>
+              <p>Web Dev</p>
+              <p>Development of multi-page and single-page web applications or websites. 
+                Excellent quality and best practices.</p>
+            </content>
+          </div>
+          <div className={styles.mystack_page_main_item}>
+            <header className={styles.mystack_page_main_item_header}>
+
+            </header>
+            <content style={styles.mystack_page_main_item_content}>
+              <p>Desktop Dev</p>
+              <p>Development of simple applications for: Windows, Mac OS, Linux.
+Using Electron.js technology.</p>
+            </content>
+          </div>
+          <div className={styles.mystack_page_main_item}>
+            <header className={styles.mystack_page_main_item_header}>
+
+            </header>
+            <content style={styles.mystack_page_main_item_content}>
+              <p>Mobile Dev</p>
+              <p>Also coming soon is mobile development using React Native...</p>
+            </content>
+          </div>
+        </main>
     </section>
   )
 }
